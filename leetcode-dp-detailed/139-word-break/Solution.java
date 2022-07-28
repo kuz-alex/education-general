6fb0c5d0ab895cd919b5a88f268b69e106117cb6
Alexander Kuznetsov <kuz-alex@yandex-team.ru>
2022-07-28 23:34:12 +0300

    139. Word Break
    https://leetcode.com/problems/word-break/

---
+++ leetcode-dp-detailed/139-word-break/Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+/**
+ * 139. Word Break
+ * https://leetcode.com/problems/word-break/
+ **/
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean dp[] = new boolean[s.length()];
+        
+        for (int i = 0; i < s.length(); ++i) {
+            for (String word : wordDict) {
+                if (i >= word.length() - 1 && (i == word.length() - 1 || dp[i - word.length()])) {
+                    if (s.substring(i - word.length() + 1, i + 1).equals(word)) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(dp));
+        return dp[s.length() - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().wordBreak("leetcode", Arrays.asList("leet", "code")));
+        System.out.println(new Solution().wordBreak("applepenapple", Arrays.asList("apple", "pen")));
+    }
+}
+
